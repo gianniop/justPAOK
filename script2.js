9c5a7db3f5e2fca56462801b116c96823c0834b6
gianniop <63250523+gianniop@users.noreply.github.com>
2020-12-10 01:37:44 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,317 @@
+class Team{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+        this.scores = [];
+    }
+}
+
+class Season{
+    constructor(teams){
+        this.teams = teams;
+        this.schedule = [];
+        this.activePhase = -1;
+    }
+
+    addScores(weekScore){
+        this.teams.forEach((team, index) => {
+            team.scores.push(weekScore[index]);
+        });
+    }
+    addPhase(phase){
+        this.schedule.push(phase);
+    }
+    startNextPhase(){
+        this.activePhase++;
+    }
+}
+class League{
+    constructor(startDay, numOfGames){
+        this.teams = [];
+        this.startDay = startDay;
+        this.table = [];
+        this.gameday = [];
+        this.activeDay = -1;
+    }
+    addTeams(teams){
+        for(let i = 0; i < teams.length; i++){
+            this.teams.push(teams[i]);
+        }
+    }
+    addGameDay(games){
+        this.gameday.push(games);
+    }
+    initTable(){
+        for(let i = 0; i < this.teams.length; i++){
+            //this.table.push([0,0]);    
+            this.table.push({ points : 0, goals : 0, id:this.teams[i].id});    
+        }
+    }
+    displayGameDay(day){
+        let element = `
+        <div class="weekDay">
+            <h3>${day + 1}η Αγωνιστική</h3>
+            <div class="fixtures center">
+                <ul>
+        `;
+        for(let i = 0; i < this.gameday[day].length; i++){
+            element += `
+                <li class="game">
+                    <div class="teams">
+                        <div class="team-info">${this.teams[this.gameday[day][i][0]].name}</div>
+                        <div class="team-info">${this.teams[this.gameday[day][i][1]].name}</div>
+                    </div>
+                    <div class="score">
+                        <div>${(day <= this.activeDay) ? this.teams[this.gameday[day][i][0]].scores[day + this.startDay] : 0 }</div>
+                        <div>${(day <= this.activeDay) ? this.teams[this.gameday[day][i][1]].scores[day + this.startDay] : 0 }</div>
+                    </div>
+                </li>` ;   
+        }
+
+        element += "</ul></div></div>";
+
+        $(".schedule").append(element);
+    }
+    displaySchedule(){
+        for(let i = 0; i < this.gameday.length; i++){
+            console.log("Day " + (i+1));
+            this.displayGameDay(i);
+        }
+    }
+    displayTable(){
+        console.log(this.table);    
+    }
+    displayTableSort(){
+        let rank = [...this.table];
+        rank.sort(function (a, b) { 
+            return (b.points != a.points ? b.points - a.points : b.goals - a.goals)});
+        //console.log(rank);
+
+        let element = '<div class="league-table prel center">';
+        for(let i = 0;i < rank.length;i++){
+            element += `
+                <div class="team lt-${(i+1)}">
+                <div class="rank">${(i+1)}</div>
+                <div class="name">${this.teams[rank[i].id].name}</div>
+                <div class="points">${rank[i].points}</div>
+                <div class="goals">${rank[i].goals}</div>
+            </div>
+            `;
+        }
+        element += "</div>";
+
+        $(element).insertAfter('.tables .table-headers');        
+    }
+    tableCalculate(){
+        console.log("games : "+this.gameday.length);
+        for(let i = 0; i <= this.activeDay; i++){
+            for(let match = 0; match < this.gameday[i].length; match++){
+                
+                if(this.teams[this.gameday[i][match][0]].scores[this.startDay + i] > this.teams[this.gameday[i][match][1]].scores[this.startDay + i]){
+                    this.table[this.gameday[i][match][0]].points += 3;
+                    const dif = this.teams[this.gameday[i][match][0]].scores[this.startDay + i] - this.teams[this.gameday[i][match][1]].scores[this.startDay + i]
+                    this.table[this.gameday[i][match][0]].goals += dif;
+                    this.table[this.gameday[i][match][1]].goals -= dif;
+                }else if(this.teams[this.gameday[i][match][0]].scores[this.startDay + i] < this.teams[this.gameday[i][match][1]].scores[this.startDay + i]){
+                    this.table[this.gameday[i][match][1]].points += 3;
+                    const dif = this.teams[this.gameday[i][match][1]].scores[this.startDay + i] - this.teams[this.gameday[i][match][0]].scores[this.startDay + i]
+                    this.table[this.gameday[i][match][1]].goals += dif;
+                    this.table[this.gameday[i][match][0]].goals -= dif;
+                }else{
+                    this.table[this.gameday[i][match][0]].points += 1;
+                    this.table[this.gameday[i][match][1]].points += 1;
+                }   
+            }
+        }
+    }
+    activeDayIncrease(){
+        this.activeDay++;
+    }
+}
+
+class SplitLeague extends League{
+    constructor(startDay, numOfGames){
+        super(startDay, numOfGames);
+        this.table = {A : [],  B : []};
+    }
+    
+    addGameDay(games){
+        let temp = [];
+        games.forEach(game => {
+            if(game.length > 1){
+                temp.push(game);
+                // 5 => length / 2
+                temp.push([game[0] + 5, game[1] + 5]);
+            }else{
+                temp.push([game[0], game[0] + 5]);
+            }
+        });
+        this.gameday.push(temp);     
+    }
+
+    initTable(){
+        for(let i = 0; i < (this.teams.length / 2); i++){
+            //this.table.push([0,0]);    
+            this.table.A.push({ points : 0, goals : 0, id:i});    
+            this.table.B.push({ points : 0, goals : 0, id:i+5});    
+        }
+    }
+    
+    tableCalculate(){
+        console.log("games : "+this.gameday.length);
+        for(let i = 0; i <= this.activeDay; i++){
+            for(let match = 0; match < this.gameday[i].length; match++){
+                //console.log("day : " + i + " - match : " + match);
+                if(this.teams[this.gameday[i][match][0]].scores[this.startDay + i] > this.teams[this.gameday[i][match][1]].scores[this.startDay + i]){
+                    //this.table[this.gameday[i][match][0]].points += 3;
+                    const dif = this.teams[this.gameday[i][match][0]].scores[this.startDay + i] - this.teams[this.gameday[i][match][1]].scores[this.startDay + i]
+                    if(this.gameday[i][match][0] < 5){
+                       // console.log("home win - gr A");
+                        this.table.A[this.gameday[i][match][0]].points += 3; 
+                        this.table.A[this.gameday[i][match][0]].goals += dif;   
+                    }else{
+                        //console.log("home win - gr B");
+
+                        this.table.B[(this.gameday[i][match][0]-5)].points += 3; 
+                        this.table.B[(this.gameday[i][match][0]-5)].goals += dif;    
+                    }
+                    if(this.gameday[i][match][1] < 5){
+                        //console.log("away lose - gr A");
+                        this.table.A[this.gameday[i][match][1]].goals -= dif;
+                    }else{
+                        //console.log("away lose - gr b");
+                        this.table.B[(this.gameday[i][match][1]-5)].goals -= dif;
+                    }
+                    
+                }else if(this.teams[this.gameday[i][match][0]].scores[this.startDay + i] < this.teams[this.gameday[i][match][1]].scores[this.startDay + i]){  
+                    const dif = this.teams[this.gameday[i][match][1]].scores[this.startDay + i] - this.teams[this.gameday[i][match][0]].scores[this.startDay + i]
+                    if(this.gameday[i][match][1] < 5){
+                        //console.log("away win - gr A")
+                        this.table.A[this.gameday[i][match][1]].points += 3; 
+                        this.table.A[this.gameday[i][match][1]].goals += dif;   
+                    }else{
+                       // console.log("away win - gr B")
+                        this.table.B[(this.gameday[i][match][1]-5)].points += 3; 
+                        this.table.B[(this.gameday[i][match][1]-5)].goals += dif;    
+                    }
+                    if(this.gameday[i][match][0] < 5){
+                        //console.log("home lose - gr A")
+                        this.table.A[this.gameday[i][match][0]].goals -= dif;
+                    }else{
+                        //console.log("home lose - gr B")
+                        this.table.B[(this.gameday[i][match][0]-5)].goals -= dif;
+                    }
+                }else{
+                    
+                    if(this.gameday[i][match][0] < 5){
+                        //console.log("draw - gr A");
+                        this.table.A[this.gameday[i][match][0]].points += 1;
+                        this.table.A[this.gameday[i][match][1]].points += 1;
+                    }else{
+                        //console.log("draw - gr B");
+                        this.table.B[(this.gameday[i][match][0]-5)].points += 1;
+                        this.table.B[(this.gameday[i][match][1]-5)].points += 1;
+                    }
+                }   
+            }
+        }    
+    }
+
+    displayTable(){
+        console.log(this.table.A);    
+        console.log(this.table.B);    
+    }
+    displayTableSort(){
+        const table = Object.entries(this.table);
+        //console.log(table);
+
+        for(let i = 0; i < table.length; i++){
+            let rank = [...table[i][1]];
+            rank.sort(function (a, b) { 
+                return (b.points != a.points ? b.points - a.points : b.goals - a.goals)});    
+            //console.log(rank);
+        
+            let element = '<div class="league-table prel center">';
+            for(let i = 0;i < rank.length;i++){
+                element += `
+                    <div class="team lt-${(i+1)}">
+                    <div class="rank">${(i+1)}</div>
+                    <div class="name">${this.teams[rank[i].id].name}</div>
+                    <div class="points">${rank[i].points}</div>
+                    <div class="goals">${rank[i].goals}</div>
+                </div>
+                `;
+            }
+            element += "</div>";
+
+            $(element).insertAfter('.tables .table-headers'); 
+        }
+        
+    }
+}
+
+let pet     = new Team(0, "Petoria Rotters");
+let seven   = new Team(1, "Eptahori FC");
+let bobo    = new Team(2, "Superbobo");
+let pali    = new Team(3, "P@li");
+let ro      = new Team(4, "Roulis FC");
+let men     = new Team(5, "Lukakos");
+let sid     = new Team(6, "Kailar");
+let tosk    = new Team(7, "Den Isxyei");
+let tsak    = new Team(8, "Dynamo Podilatou");
+let mar     = new Team(9, "Alkoolikos Asteras");
+
+const seasonOneTeams = [pet, seven, bobo, pali, ro, men, sid, tosk, tsak, mar];
+let seasonOne = new Season(seasonOneTeams);
+
+let prelimRound = new League(0, 4);
+seasonOne.addPhase(prelimRound);
+
+seasonOne.addScores([55, 63, 56, 81, 61, 69, 55, 75, 56, 70]);
+seasonOne.addScores([75, 58, 66, 84, 77, 58, 76, 71, 50, 66]);
+seasonOne.addScores([57, 65, 45, 62, 65, 52, 40, 75, 30, 53]);
+seasonOne.addScores([90, 71, 79, 59, 71, 58, 49, 41, 65, 79]);
+
+
+seasonOne.schedule[0].addTeams(seasonOne.teams);
+seasonOne.schedule[0].initTable();
+/*
+let splitRound = new SplitLeague(0, 4);
+seasonOne.addPhase(splitRound);
+
+
+seasonOne.schedule[0].addTeams([pet, seven, tosk, pali, ro]);
+seasonOne.schedule[0].addTeams([men, sid, bobo , tsak, mar]);
+seasonOne.schedule[0].initTable();
+
+seasonOne.schedule[0].addGameDay([[0, 1], [2, 3], [4]]);
+seasonOne.schedule[0].addGameDay([[0, 2], [1, 4], [3]]);
+seasonOne.schedule[0].addGameDay([[0, 4], [1, 3], [2]]);
+seasonOne.schedule[0].addGameDay([[0, 3], [2, 4], [1]]);
+seasonOne.schedule[0].addGameDay([[1, 2], [3, 4], [0]]);
+
+
+seasonOne.schedule[0].activeDayIncrease();
+seasonOne.schedule[0].activeDayIncrease();
+seasonOne.schedule[0].activeDayIncrease();
+seasonOne.schedule[0].activeDayIncrease();
+
+seasonOne.schedule[0].tableCalculate();
+seasonOne.schedule[0].displayTable();
+//seasonOne.schedule[0].displayTableSort();
+
+*/
+
+seasonOne.schedule[0].addGameDay([[0, 8], [2, 4], [5, 1], [6, 9], [3, 7]]);
+seasonOne.schedule[0].addGameDay([[2, 1], [0, 4], [5, 9], [6, 3], [7, 8]]);
+seasonOne.schedule[0].addGameDay([[0, 1], [2, 9], [5, 3], [6, 8], [4, 7]]);
+seasonOne.schedule[0].addGameDay([[0, 9], [2, 3], [5, 8], [6, 4], [7, 1]]);
+
+seasonOne.schedule[0].activeDayIncrease();
+seasonOne.schedule[0].activeDayIncrease();
+seasonOne.schedule[0].activeDayIncrease();
+seasonOne.schedule[0].activeDayIncrease();
+
+seasonOne.schedule[0].tableCalculate();
+
+
